b8427f1507b5ec94f506ad4edb3eb623294405ff
Isaac Augusto <isaac.augusto@exitgames.com>
2025-03-20 20:54:45 -0300

    Add utility and git ignore

---
+++ Utility.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTracker;
+
+public static class Utility
+{
+    // Print a message and read a string from the console
+    public static string PrintAndRead(string message)
+    {
+        Console.WriteLine(message);
+        return Console.ReadLine();
+    }
+}
